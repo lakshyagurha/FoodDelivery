d81bdd7dae87ee87174f5b9b4fd1e413c6ce9e6f
lakshyagurha <82705139+lakshyagurha@users.noreply.github.com>
2023-11-02 18:07:05 +0530

    GoodEats First Commit

--- server/Router/router.js
+++ server/Router/router.js
@@ -7,14 +7,10 @@ const middleware = require('../MiddleWare/middleware')
 
 
 router.post('/signup', signupPageController)
-
 router.post('/loginUser', loginPageController)
-
 router.post('/', middleware, mainPagePostController);
 router.get('/getData', getDataController);
-
 router.post('/orderData', userOrderDataController)
-
 router.post('/myOrderData', myOrderDataController)
 router.post('/myorders', myOrdersController)
 
